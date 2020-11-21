6df4a72b888d424c7cc133ff2d28d4ace3f43edc
brynnchernosky <56202540+brynnchernosky@users.noreply.github.com>
2020-11-21 11:40:00 -0500

    u

--- src/attention.py
+++ src/attention.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 def attention(model, decoder_hidden_state, encoder_output):
     #creates attention distribution
     score = tf.concat([decoder_hidden_state,encoder_output],0)
-    score = model.attention_weights1(cat)
-    score = model.attention_weights2(out)
+    score = model.attention_weights1(score)
+    score = model.attention_weights2(score)
     distribution = tf.nn.softmax(score)
 
     #produce attentive read from attention distribution
