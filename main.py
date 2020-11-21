b0805539d8d55b7194cd717cb86e61fac0f68850
brynnchernosky <56202540+brynnchernosky@users.noreply.github.com>
2020-11-21 10:47:17 -0500

    outline

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import tensorflow as tf
+
+def train(model, train_french, train_english, eng_padding_index):
+	"""
+	Runs through one epoch - all training examples.
+
+	:param model: the initialized model to use for forward and backward pass
+	:param train_french: french train data (all data for training) of shape (num_sentences, 14)
+	:param train_english: english train data (all data for training) of shape (num_sentences, 15)
+	:param eng_padding_index: the padding index, the id of *PAD* token. This integer is used when masking padding labels.
+	:return: None
+	"""
+    #TO DO
+    return None
+
+def test(model, test_french, test_english, eng_padding_index):
+	"""
+	Runs through one epoch - all testing examples.
+
+	:param model: the initialized model to use for forward and backward pass
+	:param test_french: french test data (all data for testing) of shape (num_sentences, 14)
+	:param test_english: english test data (all data for testing) of shape (num_sentences, 15)
+	:param eng_padding_index: the padding index, the id of *PAD* token. This integer is used when masking padding labels.
+	:returns: a tuple containing at index 0 the perplexity of the test set and at index 1 the per symbol accuracy on test set, 
+	e.g. (my_perplexity, my_accuracy)
+	"""
+    #TO DO
+	return None,None
+
+def main():
+    #TO DO - preprocess, generate model, train, test
+
+if __name__ == '__main__':
+	main()
---
+++ model.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+
+class Seq2Seq(tf.keras.Model):
+	def __init__(self, french_window_size, french_vocab_size, english_window_size, english_vocab_size):
+		super(RNN_Seq2Seq, self).__init__()
+        #TO DO
+
+    @tf.function
+    def call(self, encoder_input, decoder_input):
+		"""
+		:param encoder_input: batched ids corresponding to french sentences
+		:param decoder_input: batched ids corresponding to english sentences
+		:return prbs: The 3d probabilities as a tensor, [batch_size x window_size x english_vocab_size]
+		"""
+	
+		# TO DO
+
+    def attention(self, decoder_hidden_state, encoder_output):
+        #TO DO, return attentive read
+
+    def scratchpad(self, decoder_hidden_state,  encoder_output, attentive_read)
+        #TO DO, return updated encoder output
+
+    def accuracy_function(self, prbs, labels, mask):
+        #TO DO
+		"""
+		DO NOT CHANGE
+
+		Computes the batch accuracy
+		
+		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
+		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
+		:return: scalar tensor of accuracy of the batch between 0 and 1
+		"""
+
+    def loss_function(self, prbs, labels, mask):
+		"""
+		Calculates the total model cross-entropy loss after one forward pass. 
+		Please use reduce sum here instead of reduce mean to make things easier in calculating per symbol accuracy.
+		
+		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
+		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
+		:return: the loss of the model as a tensor
+		"""
+        #TO DO
